142104c1125d33d8bf346ebfa88f47c74e95a0e5
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-09-03 13:38:50 -0600

    con puerto

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-// app.listen(3000, () => {
-//  console.log("El servidor está inicializado en el puerto 3000");
-// });
+
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
 
 app.get('/', function (req, res) {
     res.send('Saludos desde express 4');
