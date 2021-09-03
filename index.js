90f77788326784d8895b3f501bc0eeaf4857100e
Lex Aguilar <alejandro.aguilar@eurekani.com>
2021-09-03 13:46:12 -0600

    add port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000; 
+
+app.listen(port, () => {
  console.log("El servidor está inicializado en el puerto 3000");
 });
 
